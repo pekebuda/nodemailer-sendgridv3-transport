4ae075ee512e4fa7c76089a054f08b4ea23e68ef
Fernando Diaz Laclaustra <f.diazlaclaustra@umlaud.es>
2017-08-10 18:11:27 +0200

    Adaptado codigo a mis convenciones de estilo.

--- examples/simple.js
+++ examples/simple.js
@@ -1,14 +1,18 @@
-var nodemailer = require('nodemailer');
-var sgTransport = require('../src/sendgrid-transport.js');
+var nodemailer  = require('nodemailer')
+,   sgTransport = require('../src/sendgrid-transport.js')
+;
+
+
+
 
 var options = {
     auth: {
         api_user: process.env['SENDGRID_USERNAME'],
         api_key: process.env['SENDGRID_PASSWORD']
     }
-}
-
+};
 var mailer = nodemailer.createTransport(sgTransport(options));
+
 
 var email = {
     to: ['foo@example.com', 'bar@example.com'],
@@ -16,17 +20,10 @@ var email = {
     subject: 'Hi there',
     text: 'Awesome sauce',
     html: '<b>Awesome sauce</b>',
-    attachments: [
-      {
-        filename: 'test.txt',
-        path: __dirname + '/test.txt'
-      }
-    ]
+    attachments: [ {filename: 'test.txt', path: __dirname + '/test.txt'} ]
 };
-
 mailer.sendMail(email, function(err, res) {
-    if (err) { 
-        console.log(err) 
+        if (err) console.log(err);
+        console.log(res);
     }
-    console.log(res);
-});
+);
